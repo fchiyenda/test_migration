fc3a668fa25a0c8af13b45e743a89a26aeab39cc
Fredrick Chiyenda <fchiyenda@gmail.com>
2017-10-25 15:45:29 +0200

    Added logic to check only the paticular replication to check_replication.rb script

--- check_replication.rb
+++ check_replication.rb
@@ -7,15 +7,29 @@ def replication_status(u,p,src,dst)
 	check_replication_status = RestClient.get("#{u}:#{p}@localhost:5984/_active_tasks")
   status = JSON.parse(check_replication_status)
   if status.size == 0 then
-  	#start replication
   	begin
-    	RestClient.post("http://#{u}:#{p}@localhost:5984/_replicate","{\"source\":\"#{src}\",\"target\":\"#{dst}\", \"continuous\":true}",content_type: :json)
+		 	rep_id = RestClient.post("http://#{u}:#{p}@localhost:5984/_replicate","{\"source\":\"#{src}\",\"target\":\"#{dst}\", \"continuous\":true}",content_type: :json)
+		 	$rep_id = JSON.parse(rep_id)
+		 	$rep_id = $rep_id['_local_id']
 		rescue => e
 			puts e.response
+		end	
+		 	$num += 1
+  else
+  	repli_id = status.map{|v|v['replication_id']}
+	  if !repli_id.include?("#{$rep_id}") then
+	  	 puts "I am here"
+		  	#start replication
+		  	begin
+		    	rep_id = RestClient.post("http://#{u}:#{p}@localhost:5984/_replicate","{\"source\":\"#{src}\",\"target\":\"#{dst}\", \"continuous\":true}",content_type: :json)
+		    	$rep_id = JSON.parse(rep_id)
+		    	$rep_id = $rep_id['_local_id']
+				rescue => e
+					puts e.response
+				end	
+		   	$num += 1
 		end
-	
-   	$num += 1
-  end
+	end
 end
 
 u = ARGV[0]
@@ -23,10 +37,13 @@ p = ARGV[1]
 src = ARGV[2]
 dst = ARGV[3]
 
+#Initialize variables
 i = 0
 a = ['|','/','-','\\']
 n = 0
 $num = 0
+$rep_id = ""
+
 while i != -1
 	sleep (1)
 	replication_status(u,p,src,dst)
