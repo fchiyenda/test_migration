5b5403bd8f42cad4ce9c6f22547ba408a717872c
Fredrick Chiyenda <fchiyenda@gmail.com>
2018-01-24 16:26:48 +0200

    Update

--- unassigned_npid.rb
+++ unassigned_npid.rb
@@ -45,10 +45,10 @@ def get_source_data(h,cdbusr,cdbpwd,cdb)
 
 
       primary_npids.each do |npid|
-        #puts "Checking #{npid}"
+        puts "Checking #{npid}"
           tested_assigned << npid
           es_client = client.search index:'dde',type:'npids', body:{query:{match:{ national_id: npid}}}
-          npid_decimal_value = es_client['hits']['hits'][0]['_id'] rescue nil
+          npid_decimal_value = es_client['hits']['hits'][0]['_id']
 
          #puts 'Checked if it is marked as assigned'
           if es_client['hits']['hits'][0]['_source'].has_key?('assigned')
